2cf714c0b50c25932a1577e1d005784a3dd9f3da
wildanoo <wabimantoro@gmail.com>
2023-07-30 22:28:07 +0700

    add dynamic and change to bcryptjs

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt";
+import bcryptjs from "bcryptjs";
 import prisma from "@/app/libs/prismadb";
 import { NextResponse } from "next/server";
 
@@ -6,7 +6,7 @@ export async function POST(request: Request) {
    const body = await request.json();
    const { email, name, password } = body;
 
-   const hashedPassword = await bcrypt.hash(password, 12);
+   const hashedPassword = await bcryptjs.hash(password, 12);
    const user = await prisma.user.create({
       data: {
          email,
--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+   experimental: {
+      appDir: true,
+   },
    images: {
       domains: ["avatars.githubusercontent.com", "lh3.googleusercontent.com", "res.cloudinary.com"],
    },
